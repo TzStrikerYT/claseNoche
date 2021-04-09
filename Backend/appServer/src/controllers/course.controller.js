bb089c1644d18800f216e876e8fadd8b3aff0138
TzStrikerYT <maicol.asus@gmail.com>
2021-04-08 20:17:27 -0500

    [update] adicion de controllers

---
+++ Backend/appServer/src/controllers/course.controller.js
@@ -0,0 +1,7 @@
+import Course from '../models/Course'
+
+export const createCourse = () => {}
+export const getCourses = () => {} // todos los cursos
+export const getCourseById = () => {} // un curso especifico
+export const updateCourse = () => {}
+export const deleteCourse = () => {}
