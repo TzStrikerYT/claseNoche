6a8df61d1f2c0f661a24e703799f7b44b5706d71
TzStrikerYT <maicol.asus@gmail.com>
2021-04-16 11:34:45 -0500

    [Del] delete trash in repository

--- Backend/appServer/src/controllers/course.controller.js
+++
@@ -1,9 +0,0 @@
-import Course from '../models/Course'
-
-export const createCourse = (req, res) => {
-    res.send("Creado")
-}
-export const getCourses = () => {} // todos los cursos
-export const getCourseById = () => {} // un curso especifico
-export const updateCourse = () => {}
-export const deleteCourse = () => {}
--- Backend/appServer/src/routes/course.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express'
-
-const router = Router()
-
-import * as courseController from '../controllers/course.controller'
-
-router.get('/', courseController.createCourse)
-
-export default router
