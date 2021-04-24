994f6be1ef0a0acaa18a93db91b2e3dd57d71145
Michael Cocuy <maicol.asus@gmail.com>
2021-04-24 01:02:20 -0500

    [add] update of class

--- appCompleta/Frontend/src/app/app.module.ts
+++ appCompleta/Frontend/src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http' /* Pasdo 1 importar  */
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { PagErrorComponent } from './components/pag-error/pag-error.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PagErrorComponent,
-    CardsComponent
+    CardsComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule, /* LLamar */
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- appCompleta/Frontend/src/app/models/Product.ts
+++ appCompleta/Frontend/src/app/models/Product.ts
@@ -1,11 +1,11 @@
 export interface Product {
-    _id: string;
+    _id?: string;
     name: string;
     price: string;
     category: string;
     brand: string;
     imgUrl: string;
-    createdAt: string;
-    updatedAt: string;
+    createdAt?: string;
+    updatedAt?: string;
     /* ? opcional */
 }
